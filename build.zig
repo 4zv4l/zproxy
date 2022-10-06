8ec00544900e28fed0d2163d824592e4e2b4820c
azz <4zv4l@protonmail.com>
2022-10-06 22:36:18 +0200

    change entry point

--- build.zig
+++ build.zig
@@ -11,7 +11,7 @@ pub fn build(b: *std.build.Builder) void {
     // between Debug, ReleaseSafe, ReleaseFast, and ReleaseSmall.
     const mode = b.standardReleaseOptions();
 
-    const exe = b.addExecutable("proxy", "src/main.zig");
+    const exe = b.addExecutable("proxy", "src/proxy.zig");
     exe.setTarget(target);
     exe.setBuildMode(mode);
     exe.strip = true;
